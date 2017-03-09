bd740c3772ca882b2e6f96677ab0c9b2816171bf
roseliux <roxdurazo@gmail.com>
2017-03-09 13:16:41 -0700

    Create minitest

--- Lesson 4/FrogRiverOne.rb
+++ Lesson 4/FrogRiverOne.rb
@@ -11,3 +11,19 @@ def solution(x, a)
   end
   -1
 end
+
+require 'minitest/autorun'
+
+class Tests < Minitest::Test
+  def test_example_input
+    assert_equal 6, solution(5, [1, 3, 1, 4, 2, 3, 5, 4])
+  end
+
+  def test_not_possible
+    assert_equal -1, solution(5, [1, 2, 1, 2, 1, 4, 5])
+  end
+
+  def test_immediately
+    assert_equal 0, solution(1, [1, 1])
+  end
+end
